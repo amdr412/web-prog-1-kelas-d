d965a2537df304bba1b4735579b57440426ad375
AR-412 <36441230+ropii@users.noreply.github.com>
2018-02-27 17:14:42 +0700

    Update Materi Pertemuan 2 Array

---
+++ pertemuan2/larik-modif.php
@@ -0,0 +1,11 @@
+<?php
+$data = array(
+     "Nama" => "tamami",
+     "Usia" => 29,
+     "Jomlo" => true
+ );
+
+ echo "NAMA : " . $data["Nama"] . "<br>";
+ echo "USIA : " . $data["Usia"] . "<br>";
+ echo "JOMLO ? " . $data["Jomlo"] . "<br>";
+ ?>
---
+++ pertemuan2/larik-multi.php
@@ -0,0 +1,21 @@
+<?php
+
+$data = array(
+
+       "web_prog" =>
+       array(
+            "ropii" => 5,
+            "andi" => 7,
+             "yoga" => 9),
+       "kalkulus" =>
+       array(
+            "yoga" => 6,
+            "andi" => 8,
+            "ropii" => 7)
+ );
+ 
+ echo "Nilai Web-Prog Andi : " .
+    $data["web_prog"]["andi"] . "<br>"; 
+ echo "Nilai Kalkulus Yoga : " .
+   $data["kalkulus"]["yoga"] . "<br>"; 
+   ?>       
